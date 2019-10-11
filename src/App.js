7421cc0018dc1cd5dc4218857d151ca3184e00f4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-11 01:22:16 -0400

    Updates the displayed text in <App />

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Hello, World!</h1>
+      <h1>Welcome to the *NEW* Elkton Missionary Church Website!</h1>
+      <h2>Unfortunately, masterpieces take time. 😞</h2>
+      <h3>
+        However, we hope you do check back soon for the launch of v1.0! 🚀{' '}
+      </h3>
     </div>
   );
 }
