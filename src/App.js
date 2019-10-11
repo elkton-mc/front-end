d8c177284fdbdfac3a11a87d4537429610332161
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-11 01:45:51 -0400

    Removes warnings in the console regarding unwrapped emojis by adding <span> elements

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ function App() {
   return (
     <div className='App'>
       <h1>Welcome to the *NEW* Elkton Missionary Church Website!</h1>
-      <h2>Unfortunately, masterpieces take time. 😞</h2>
+      <h2>
+        Unfortunately, masterpieces take time.
+        <span role='img' aria-label='emoji'>
+          😞
+        </span>
+      </h2>
       <h3>
-        However, we hope you do check back soon for the launch of v1.0! 🚀{' '}
+        However, we do hope you check back soon for the launch of v1.0!{' '}
+        <span role='img' aria-label='emoji'>
+          🚀
+        </span>
       </h3>
     </div>
   );
